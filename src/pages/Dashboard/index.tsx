6fac289091beb2803ed36c14407eec67d59e87d9
Ítalo Gomes <47856024+italoog@users.noreply.github.com>
2021-01-30 22:10:34 -0300

    Conectando a API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,64 +1,67 @@
-import React from 'react';
+/* eslint-disable camelcase */
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repoditório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repoditório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/47856024?s=400&u=a82527cbb19e8ae5a35614de33a2deb5843d290d&v=4"
-            alt="Italo Gomes"
-          />
-          <div>
-            <strong>italoog/gostack-gobarber-backend</strong>
-            <p>
-              Aplicação desenvolvida durante o bootcamp GoStack da Rocketseat
-            </p>
-          </div>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/47856024?s=400&u=a82527cbb19e8ae5a35614de33a2deb5843d290d&v=4"
-            alt="Italo Gomes"
-          />
-          <div>
-            <strong>italoog/gostack-gobarber-backend</strong>
-            <p>
-              Aplicação desenvolvida durante o bootcamp GoStack da Rocketseat
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/47856024?s=400&u=a82527cbb19e8ae5a35614de33a2deb5843d290d&v=4"
-            alt="Italo Gomes"
-          />
-          <div>
-            <strong>italoog/gostack-gobarber-backend</strong>
-            <p>
-              Aplicação desenvolvida durante o bootcamp GoStack da Rocketseat
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
