0fe9aac7fe6e0a60379e8317bffbf16669953f09
Ítalo Gomes <47856024+italoog@users.noreply.github.com>
2021-02-02 17:54:23 -0300

    1.25

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +13,27 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository:{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/dachboard">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars.githubusercontent.com/u/47856024?s=460&u=a82527cbb19e8ae5a35614de33a2deb5843d290d&v=4"
+            alt="Italo Gomes"
+          />
+        </header>
+        <ul />
+      </RepositoryInfo>
+    </>
+  );
 };
 
 export default Repository;
